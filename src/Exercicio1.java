0ec4311faba6240a2129a44661643facb8b8843a
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-09-19 09:06:37 -0300

    add exercicio 2

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -6,6 +6,7 @@ public class Exercicio1 {
         private String nome;
         private BigDecimal preco;
         private BigDecimal qtdEstoque;
+        private BigDecimal desconto;
 
         public String getNome() {
             return this.nome;
@@ -39,6 +40,29 @@ public class Exercicio1 {
             }
             this.qtdEstoque = qtdEstoque;
         }
+
+        public BigDecimal getDesconto() {
+            return this.desconto;
+        }
+
+        public void setDesconto(BigDecimal desconto) {
+            this.desconto = desconto;
+        }
+
+        public void aplicarDesconto(double porcentagem) {
+            BigDecimal precoAtual = this.getPreco();
+            BigDecimal porcentagemBD = BigDecimal.valueOf(porcentagem);
+            this.setDesconto(porcentagemBD);
+
+            if (porcentagemBD.compareTo(BigDecimal.ZERO) < 0 || porcentagemBD.compareTo(BigDecimal.valueOf(50)) > 0) {
+                throw new IllegalArgumentException("Valor de desconto deve estar entre 0 e 50%");
+            }
+
+            BigDecimal fatorDesconto = BigDecimal.ONE.subtract(porcentagemBD.divide(BigDecimal.valueOf(100)));
+
+            BigDecimal novoPreco = precoAtual.multiply(fatorDesconto);
+            this.setPreco(novoPreco);
+        }
     }
 
     public static void main(String[] args) {
@@ -49,13 +73,17 @@ public class Exercicio1 {
         // produto1.setQtdEstoque(BigDecimal.valueOf(-2));
 
         produto1.setNome("Celular");
-        produto1.setPreco(BigDecimal.valueOf(1800.00));
+        produto1.setPreco(BigDecimal.valueOf(100));
         produto1.setQtdEstoque(BigDecimal.valueOf(2));
 
         System.out.println("Nome do Produto: " + produto1.getNome());
         System.out.println("Preço: R$" + produto1.getPreco());
         System.out.println("Quantidade em estoque: " + produto1.getQtdEstoque());
 
+        //produto1.aplicarDesconto(-1);
+        //produto1.aplicarDesconto(51);
+        produto1.aplicarDesconto(10);
+        System.out.println("Produto " + produto1.getNome() + " com " + produto1.getDesconto() + "% de desconto: R$" + produto1.getPreco());
     }
 
 }
